d055d6ad9c5ae241fccfca3a73a829df03115b4f
Prashant Rawat <prashantxrawat@gmail.com>
2023-04-28 13:45:10 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  collectCoverageFrom: [
-    './components/*.js', // include all .js files in the src directory and its subdirectories
-  ],
-  // other Jest options...
-    "coverageThreshold": {
-        "global": {
-            "lines": 50
-        }
+  collectCoverageFrom: ["./components/*.js"],
+  coverageThreshold: {
+    global: {
+      lines: 50
     }
+  }
+  // other Jest options...
+//     coverageThreshold: {
+//         "global": {
+//             "lines": 50
+//         }
+//     }
 };
