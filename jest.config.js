622cebaa039f2f82ed952c6f4650588a407394fd
Prashant Rawat <prashantxrawat@gmail.com>
2023-04-28 13:34:27 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
       './components/*.js', // include all .js files in the src directory and its subdirectories
     ],
     // other Jest options...
+    "coverageThreshold": {
+        "global": {
+            "lines": 50
+        }
+    }
   };
   
